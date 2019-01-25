6b0ccab9df7b313a32c6cd1c6812cde75dcd7398
mahil2018 <mariahromeroa@yahoo.com>
2019-01-24 23:15:59 -0500

    Finish iteration 3

--- module2/006-mongoose-recipes/recipes.js
+++ module2/006-mongoose-recipes/recipes.js
@@ -5,33 +5,43 @@ const data = require('./data.js');
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');
-  }).catch(err => {
+   })
+  .catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
   //Iteration 1 - Recipe Schema
- const catSchema = new Schema({
+ const recipeSchema = new Schema({
   title : {type: String, required: true, unique: true},
-  level: {type: ENUM["Easy Peasy", "Amateur Chef", "UltraPro Chef"], 
-  ingredients:ENUM,
+  level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'], 
+  ingredients: [], 
   cuisine : {type: String, required: true},
-  dishType: {type:String, ENUM:["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]},
+  dishType: {type:String, enum:['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
   image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
-  duration: {type: number, default:0},
+  duration: {type: Number, default:0},
   creator: {type: String},
-  created: {type: Date, default: today}
+  created: {type: Date, default: Date.now}
  }
 });
+//connect the schema with the model we are exporting
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
 
 //Create a recipe
 
-recipe.create({ title: this.title, level: this.level, ingredients:this.ingredients, cuisine: this.cuisine, dishType: dishType.this, image: image.this, duration: duration.this, creator: creator.this, created:created.this},
-   function (err, recipe) {
-  if (err) console.log('An error happened:', err);
-  else console.log('The title of the recipe is: ', recipe.title);
-});
+Recipe.create({
+    title: 'Sancocho',
+    level: 'UltraPro Chef',
+    ingredients: ['16 cups water’, ‘1 Hen despresada’, ‘1 Pound (500gr.) Of ribs of beef, chopped’, ‘1 Pound (500gr.) Of pork ribs, chopped’, ‘4 Chorizos, matches’, ‘2 Cloves of garlic, fine chopped’, ‘3 whole onion stems’,‘2 ripe tomatoes, peeled and chopped’, ‘2 Green bananas, peeled and split by hand’, ‘1 Pound (500 gr.) Of cassava, peeled and split’, ‘1/2 Pound (250 gr.) Of celery (arracacha), peeled and chopped’, ‘2 Cobs, pieces in pieces’, ‘1/2 Pound (250gr.) Potatoes, small, peeled’, ‘3 Cabbage leaves, chopped’, ‘1 Ripe banana, chopped into pieces with peel’, ‘1/4 Pound (125 gr.) Of soaked chickpeas’, ‘1/4 Pound (125 gr.) Of smoke’, ‘1 grated carrot’, ‘1/4 cup of washed rice’, ‘1 ½ Cup of chorotas ’, ‘Salt, pepper and cumin to taste'],
+    cuisine: 'Colombian',
+    dishType: ['Dish'],
+    image: 'http://www.elcampesino.co/wp-content/uploads/2017/05/Sancocho.jpg',
+    duration: 40,
+    creator: 'Chef Abue'
+  });
 
-// Same code but with a Promise version
-User.create({ name: 'Alice', job: 'Architect' })
-  .then((user) => { console.log('The user is saved and its value is: ', user) })
-  .catch((err) => { console.log('An error happened:', err) });
+Iteration 4 - Update recipe
+Recipe.insertMany(data);
+
+
+ 
