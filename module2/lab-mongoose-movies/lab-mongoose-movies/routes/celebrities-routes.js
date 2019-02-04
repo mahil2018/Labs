7517333e7e10cac6ccbd41c1d45f5c3f0870cdd4
mahil2018 <mariahromeroa@yahoo.com>
2019-02-04 16:08:02 -0500

    almost 50

--- module2/lab-mongoose-movies/lab-mongoose-movies/routes/celebrities-routes.js
+++ module2/lab-mongoose-movies/lab-mongoose-movies/routes/celebrities-routes.js
@@ -1,5 +1,6 @@
 const express   = require('express');
 const Celebrity = require('../models/celebrity-model');
+// const Celebrity = require('../models/celebrity-model');
 const celebrityRouter     = express.Router();
 
 
@@ -39,17 +40,63 @@ celebrityRouter.post('/', (req, res, next) => {
     Celebrity.find()
     .then(allCelebrities =>{
       console.log('Our list of celebrities ares: ', allCelebrities)
-      res.render('celebrities/index', {celebrities: allCelebrities})
+      res.render('celebrities/celebrities', {allCelebrities})
+    })
     .catch(err => { console.log('Error while creating a new celebrity: ', err)});
     })
 
-  })
+  
 //====================
 
-// //Create celebrities
-// celebrityRouter.get('/create', (req, res, next) =>{
-//   res.render('celebrities/new')
-// })
+//See details of a celebrity
+//localhost:3000/celebrities/5c579264643c2ff3f7d61b33
+celebrityRouter.get('/:celebrityId', (req, res, next) => {
+  // console.log('The celebrity Id: ', req.params.celebrityId)
+  Celebrity.findById(req.params.celebrityId)
+  .then(celebrityOne =>{
+    // console.log('this is the celebrity:', celebrityOne);
+    res.render('celebrities/celebrity-details', {details: celebrityOne})
+  })
+  .catch(err => { console.log('Error while showing a celebrity: ', err)});
+  })
+
+//=================
+//Update Existing celebrities
+celebrityRouter.get('/modify/:id', (req, res, next) =>{
+  // console.log('update id:', req.params.id);
+  Celebrity.findById(req.params.id)
+  .then(celebrityUpdate =>{
+    // console.log('this is the celebrity:', celebrityOne);
+    res.render('celebrities/celebrity-update', {celebrityUpdate})
+  })
+  .catch(err => { console.log('Error while showing a celebrity: ', err)});
+  })
+
+//localhost:3000/celebrities/modify/
+//localhost:3000/celebrities/modify/5c579264643c2ff3f7d61b33
+celebrityRouter.post('/modify/:id', (req,res,next) =>{
+  // console.log('modificando a : ', req.body);
+  // console.log('show me id:', req.params.id);
+  Celebrity.findByIdAndUpdate(req.params.id, req.body)
+  .then(changeCel =>{
+    res.redirect(`/celebrities/${req.params.id}`)
+    // console.log('modificando a : ', req.body);
+  })
+  .catch(err => { console.log('Error while updating a celebrity: ', err)});
+
+})
+
+//delete existing celebrities
+//localhost:3000/celebrities/delete/5c579264643c2ff3f7d61b33  ..url params
+celebrityRouter.post('/delete/:id', (req, res, next) =>{
+  console.log('delete id:', req.params.id);
+  Celebrity.findByIdAndRemove(req.params.id)
+  .then(celebrityDelete =>{
+    console.log('this is the celebrity:', celebrityDelete);
+    res.redirect('/celebrities')
+  })
+  .catch(err => { console.log('Error while showing a celebrity: ', err)});
+  })
 
 
 
