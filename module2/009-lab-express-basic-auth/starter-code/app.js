71f3bc23c940fb7028830de8302b1fd9a39b8aca
mahil2018 <mariahromeroa@yahoo.com>
2019-02-08 16:43:07 -0500

    done

--- module2/009-lab-express-basic-auth/starter-code/app.js
+++ module2/009-lab-express-basic-auth/starter-code/app.js
@@ -9,9 +9,11 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
-  .connect('mongodb://localhost/lab-mongoose-movies', {useNewUrlParser: true})
+  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -29,6 +31,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//session setup
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // Express View engine setup
 
@@ -54,7 +66,10 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
-app.use('/celebrities', require('./routes/celebrities-routes'));
-app.use('/movies', require('./routes/movies-routes'));
+const router = require('./routes/auth');
+app.use('/', router);
+
+// app.use('/', require('./routes/auth-routes'));
+app.use('/', require('./routes/site-routes'));
 
 module.exports = app;
